cf8ed2be623b356da575b63d9f679f7066d38eca
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2025-03-25 11:33:10 +0300

    Updated config

--- src/config.py
+++ src/config.py
@@ -2,7 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    drone_ip0: str = "192.168.2.76"  # "192.168.0.10"
+    # drone_ip0: str = "192.168.2.76"
+    drone_ip0: str = "192.168.0.10"
     drone_port0: int = 22
     drone_mavlink_port0: int = 14550
 
@@ -11,9 +12,11 @@ class Config(BaseSettings):
     drone_mavlink_port1: int = 14550
 
     drone_username: str = "pi"
-    drone_password: str = "s-6-zmKI"  # "raspberry"
+    # drone_password: str = "s-6-zmKI"
+    drone_password: str = "raspberry"
 
-    is_one_drone: bool = True
+    # is_one_drone: bool = True
+    is_one_drone: bool = False
 
     selfcheck_timeout: int = 60
 

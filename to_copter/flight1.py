cafc20c92d158a50989a809f6bf57f22a379a10f
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2025-03-26 02:03:52 +0300

    Synced flight 1 code

--- to_copter/flight1.py
+++ to_copter/flight1.py
@@ -1,6 +1,6 @@
+import json
 import math
 import sys
-from io import BytesIO
 from threading import Thread
 from typing import Tuple
 
@@ -16,8 +16,9 @@ from sensor_msgs.msg import CameraInfo, Image
 from std_srvs.srv import Trigger
 from tf2_geometry_msgs import PointStamped
 from tf2_ros import Buffer, TransformListener
+from websockets.sync.client import connect as websocket_connect
 
-sys.stdout = open("flight1.log", "w")
+# sys.stdout = open("flight1.log", "w")
 
 rospy.init_node("flight1")
 
@@ -88,29 +89,62 @@ tf_listener = TransformListener(tf_buffer)
 camera_info = rospy.wait_for_message("main_camera/camera_info", CameraInfo)
 camera_matrix = np.reshape(np.array(camera_info.K, dtype="float64"), (3, 3))
 dist_coeffs = np.array(camera_info.D, dtype="float64")
-object_point = np.array(
+object_point_60_60 = np.array(
     [
-        (-90 / 2, -60 / 2, 0),  # Bottom-left corner  (-45, -30, 0)
-        (90 / 2, -60 / 2, 0),  # Bottom-right corner (45, -30, 0)
-        (90 / 2, 60 / 2, 0),  # Top-right corner    (45, 30, 0)
-        (-90 / 2, 60 / 2, 0),  # Top-left corner     (-45, 30, 0)
+        (-0.6 / 2, -0.6 / 2, 0),  # Bottom-left corner
+        (0.6 / 2, -0.6 / 2, 0),  # Bottom-right corner
+        (0.6 / 2, 0.6 / 2, 0),  # Top-right corner
+        (-0.6 / 2, 0.6 / 2, 0),  # Top-left corner
     ]
 )
+object_point_60_90 = np.array(
+    [
+        (-0.6 / 2, -0.9 / 2, 0),  # Bottom-left corner
+        (0.6 / 2, -0.9 / 2, 0),  # Bottom-right corner
+        (0.6 / 2, 0.9 / 2, 0),  # Top-right corner
+        (-0.6 / 2, 0.9 / 2, 0),  # Top-left corner
+    ]
+)
+object_point_90_90 = np.array(
+    [
+        (-0.9 / 2, -0.9 / 2, 0),  # Bottom-left corner
+        (0.9 / 2, -0.9 / 2, 0),  # Bottom-right corner
+        (0.9 / 2, 0.9 / 2, 0),  # Top-right corner
+        (-0.9 / 2, 0.9 / 2, 0),  # Top-left corner
+    ]
+)
+# other things
 counter = 0
 do_recognition = False
+websocket_frame = websocket_connect(
+    f"{API_BASEURL.replace('http', 'ws')}/process-frame/1"
+)
+websocket_coords = websocket_connect(
+    f"{API_BASEURL.replace('http', 'ws')}/process-coords/1"
+)
 
 
 def transform_xyz_yaw(
-    x: float, y: float, z: float, frame_from: str, frame_to: str
+    x: float, y: float, z: float, frame_from: str, frame_to: str, curr_time: rospy.Time
 ) -> Tuple[float, float, float]:
     point_from = PointStamped()
     point_from.header.frame_id = frame_from
-    point_from.header.stamp = rospy.get_rostime()
+    point_from.header.stamp = curr_time
     point_from.point.x = x
     point_from.point.y = y
     point_from.point.z = z
     point_to = tf_buffer.transform(point_from, frame_to, rospy.Duration(0.2))
     return point_to.point.x, point_to.point.y, point_to.point.z
+
+
+def polygon_area(points):
+    n = len(points)
+    area = 0.0
+    for i in range(n):
+        j = (i + 1) % n
+        area += points[i][0] * points[j][1]
+        area -= points[j][0] * points[i][1]
+    return abs(area) / 2.0
 
 
 @long_callback
@@ -120,57 +154,58 @@ def img_callback(msg):
     if not do_recognition:
         return
 
-    # counter += 1
-    # if counter % 5 != 0:
-    #     return
+    counter += 1
+    if counter % 5 != 0:
+        return
 
     try:
         image = bridge.imgmsg_to_cv2(msg, "bgr8")
+        curr_time = rospy.get_rostime()
 
         _, img_encoded = cv2.imencode(".jpg", image)
-        resp = requests.post(
-            f"{API_BASEURL}/process-frame",
-            files={"file": ("image.jpg", BytesIO(img_encoded), "image/jpeg")},
-            timeout=10,
-        )
-        if resp.status_code != 200:
-            rospy.loginfo(f"Error processing frame: {resp.status_code}")
-            return
-        coords = resp.json()
+        websocket_frame.send(img_encoded.tobytes())
+        resp = websocket_frame.recv()
+        coords = json.loads(resp)["coordinates"]
         rospy.loginfo(f"Coords: {coords}")
 
         corners_points = []
         for coord in coords:
+            image_point = [
+                (coord[0], coord[1] + coord[3]),
+                (coord[0] + coord[2], coord[1] + coord[3]),
+                (coord[0] + coord[2], coord[1]),
+                (coord[0], coord[1]),
+            ]
+            area = polygon_area(image_point)
+
+            if area <= 2000:
+                object_point = object_point_60_60
+            elif 2000 <= area <= 2500:
+                object_point = object_point_60_90
+            else:
+                object_point = object_point_90_90
+
             _, rvec, tvec = cv2.solvePnP(
                 np.array(object_point, dtype="float32"),
-                np.array(
-                    [
-                        (coord[0], coord[1] + coord[3]),
-                        (coord[0] + coord[2], coord[1] + coord[3]),
-                        (coord[0] + coord[2], coord[1]),
-                        (coord[0], coord[1]),
-                    ],
-                    dtype="float32",
-                ),
+                np.array(image_point, dtype="float32"),
                 camera_matrix,
                 dist_coeffs,
             )
 
+            R, _ = cv2.Rodrigues(rvec)
+
             corner_points = []
             for point in object_point:
-                point_3d = np.array([[point]], dtype="float32")
-                point_2d, _ = cv2.projectPoints(
-                    point_3d, rvec, tvec, camera_matrix, dist_coeffs
-                )
-
-                x_map, y_map, z_map = transform_xyz_yaw(
-                    float(point_2d[0][0][0]),  # X
-                    float(point_2d[0][0][1]),  # Y
-                    float(tvec[2][0]),  # Z (depth)
+                point_cam = (R.dot(point.reshape(3, 1))).flatten() + tvec.flatten()
+                world_pt = transform_xyz_yaw(
+                    point_cam[0],
+                    point_cam[1],
+                    point_cam[2],
                     "main_camera_optical",
                     "aruco_map",
+                    curr_time,
                 )
-                corner_points.append((x_map, y_map, z_map))
+                corner_points.append(world_pt)
 
             corners_points.append(
                 [
@@ -178,22 +213,13 @@ def img_callback(msg):
                     / 4,  # X CENTER (average of all x coordinates)
                     sum(p[1] for p in corner_points)
                     / 4,  # Y CENTER (average of all y coordinates)
-                    abs(
-                        (corner_points[1][0] - corner_points[0][0])
-                        * 100
-                        * (corner_points[2][1] - corner_points[1][1])
-                        * 100
-                    ),  # AREA (width * height)
+                    polygon_area(corner_points) * 10000,
                 ]
             )
 
         rospy.loginfo(f"Corners points: {corners_points}")
 
-        requests.post(
-            f"{API_BASEURL}/process-coords",
-            json={"coords": corners_points},
-            timeout=10,
-        )
+        websocket_coords.send(json.dumps({"coords": corners_points}))
     except Exception as e:
         rospy.loginfo(f"Error processing frame: {e}")
 
@@ -271,7 +297,9 @@ def land_wait():
 try:
     rospy.loginfo("Setting state to ready to start")
     requests.post(
-        f"{API_BASEURL}/state/1", json={"ready_to_start": True, "ready_to_land": False}
+        f"{API_BASEURL}/state/1",
+        json={"ready_to_start": True, "ready_to_land": False},
+        timeout=10,
     )
 except Exception as e:
     rospy.loginfo(f"Error setting state: {e}")
@@ -307,7 +335,9 @@ do_recognition = False
 try:
     rospy.loginfo("Setting state to ready to land")
     requests.post(
-        f"{API_BASEURL}/state/1", json={"ready_to_start": False, "ready_to_land": True}
+        f"{API_BASEURL}/state/1",
+        json={"ready_to_start": False, "ready_to_land": True},
+        timeout=10,
     )
 except Exception as e:
     rospy.loginfo(f"Error setting state: {e}")
@@ -332,8 +362,13 @@ land_wait()
 
 try:
     requests.post(
-        f"{API_BASEURL}/state/1", json={"ready_to_start": False, "ready_to_land": False}
+        f"{API_BASEURL}/state/1",
+        json={"ready_to_start": False, "ready_to_land": False},
+        timeout=10,
     )
     requests.post(f"{API_BASEURL}/reset_pids/1", timeout=10)
 except Exception as e:
     rospy.loginfo(f"Error setting final state: {e}")
+
+websocket_frame.close()
+websocket_coords.close()

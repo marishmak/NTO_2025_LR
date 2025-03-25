60e08cb6e3003bfb6fdae787f81df59aa33d84b6
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2025-03-25 22:11:59 +0300

    Updated CV

--- src/vision.py
+++ src/vision.py
@@ -4,39 +4,52 @@ import cv2
 import numpy as np
 
 
+def crop_center(image, percent):
+    h, w = image.shape[:2]
+    crop_width = int(w * percent / 100)
+    crop_height = int(h * percent / 100)
+
+    start_x = (w - crop_width) // 2
+    start_y = (h - crop_height) // 2
+
+    cropped_image = image[
+        start_y : start_y + crop_height, start_x : start_x + crop_width
+    ]
+    return cropped_image
+
+
 def image_process(img):
     hsv_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     lower_red_1 = np.array([0, 90, 90])
-    upper_red_1 = np.array([200, 255, 255])
-    lower_red_2 = np.array([0, 90, 90])
-    upper_red_2 = np.array([200, 255, 255])
-    lower_yellow_orange = np.array([0, 90, 90])
-    upper_yellow_orange = np.array([200, 255, 255])
+    upper_red_1 = np.array([10, 255, 255])
+    lower_red_2 = np.array([170, 90, 90])
+    upper_red_2 = np.array([180, 255, 255])
+    lower_orange = np.array([10, 90, 90])
+    upper_orange = np.array([25, 255, 255])
+    lower_yellow = np.array([25, 90, 90])
+    upper_yellow = np.array([35, 255, 255])
 
     mask_red_1 = cv2.inRange(hsv_image, lower_red_1, upper_red_1)
     mask_red_2 = cv2.inRange(hsv_image, lower_red_2, upper_red_2)
-    mask_yellow_orange = cv2.inRange(
-        hsv_image, lower_yellow_orange, upper_yellow_orange
-    )
+    mask_orange = cv2.inRange(hsv_image, lower_orange, upper_orange)
+    mask_yellow = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
 
     fire_mask = cv2.bitwise_or(mask_red_1, mask_red_2)
-    fire_mask = cv2.bitwise_or(fire_mask, mask_yellow_orange)
+    fire_mask = cv2.bitwise_or(fire_mask, mask_orange)
+    fire_mask = cv2.bitwise_or(fire_mask, mask_yellow)
 
     processed_img = cv2.bitwise_and(img, img, mask=fire_mask)
-    # showInRow([img, mask, processed_img], ['origin', 'mask', 'result'])
 
     return processed_img
 
 
 def thresholding(processed_img):
-    thresh_val = 90
-
     img_gray = cv2.cvtColor(processed_img, cv2.COLOR_BGR2GRAY)
     img_gray = cv2.GaussianBlur(img_gray, (11, 11), 0)
     img_gray = cv2.medianBlur(img_gray, 9)
 
-    ret, final_thresh = cv2.threshold(img_gray, thresh_val, 255, cv2.THRESH_BINARY)
+    ret, final_thresh = cv2.threshold(img_gray, 90, 255, cv2.THRESH_BINARY)
     return final_thresh
 
 
@@ -53,19 +66,7 @@ def morphological_ops(img_after_thresholding, kernel):
     dilation = cv2.dilate(erosion, kernel, iterations=2)
     img_morphology = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
 
-    # removing small noizes
-    connectivity = 1
-    n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(
-        img_morphology, connectivity, cv2.CV_32S
-    )
-
-    labeled_image = np.zeros(
-        (img_morphology.shape[0], img_morphology.shape[1], 3), dtype=np.uint8
-    )
-    labeled_image[labels == n_labels - 1, :] = [255, 255, 255]
-    img_gray = cv2.cvtColor(labeled_image, cv2.COLOR_BGR2GRAY)
-
-    return img_gray
+    return img_morphology
 
 
 def contour(fire_mask, image) -> Tuple[np.ndarray, List[Tuple[int, int, int, int]]]:
@@ -78,18 +79,9 @@ def contour(fire_mask, image) -> Tuple[np.ndarray, List[Tuple[int, int, int, int
         pixel_area = cv2.contourArea(contour)
         if pixel_area > 500:
             x, y, w, h = cv2.boundingRect(contour)
-
-            contour = contours[0]
-            M = cv2.moments(contour)
-            cx = int(M["m10"] / M["m00"])
-            cy = int(M["m01"] / M["m00"])
-            print(cx, cy)
-
-            print("Coordinates of fire:", (x + w // 2, y + h // 2))
             coordinates.append((x, y, w, h))
 
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
             text = "Fire"
             cv2.putText(
                 image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2
@@ -99,16 +91,19 @@ def contour(fire_mask, image) -> Tuple[np.ndarray, List[Tuple[int, int, int, int
 
 
 def main_fire_detection(img):
-    processed_img = image_process(img)
+    crop_percent = 90
+    cropped_img = crop_center(img, crop_percent)
+    processed_img = image_process(cropped_img)
     img_after_thresholding = thresholding(processed_img)
     kernel = define_kernel(5, 5)
     mask = morphological_ops(img_after_thresholding, kernel)
 
-    final_img, coordinates = contour(mask, img)
-    return final_img, coordinates
+    final_img, coordinates = contour(mask, cropped_img.copy())
+
+    return final_img, coordinates, mask
 
 
 if __name__ == "__main__":
-    image = cv2.imread("pic1.png")
-    final_img, coordinates = main_fire_detection(image)
-    print(coordinates)
+    name = "pic1.png"
+    image = cv2.imread(name)
+    final_img, coordinates, mask = main_fire_detection(image)
